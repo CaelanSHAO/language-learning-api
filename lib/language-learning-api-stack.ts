64d13aae8f2341d33fe584d69563f034800b4509
JIAYI SHAO <sjy08015342@163.com>
2025-03-23 00:52:26 +0000

    Add DynamoDB table with composite primary key

--- lib/language-learning-api-stack.ts
+++ lib/language-learning-api-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class LanguageLearningApiStack extends cdk.Stack {
+  public readonly table: dynamodb.Table;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 创建 DynamoDB 表
+    const table = new dynamodb.Table(this, 'PhrasesTable', {
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'phraseId', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // ⚠️ 开发阶段用，正式时改为 RETAIN
+      tableName: 'PhrasesTable'
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LanguageLearningApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    this.table = table;
   }
 }
